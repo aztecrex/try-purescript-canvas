032b7ba7232dc1b6791e99cca7912873171ba7e1
Greg Wiley <aztec.rex@jammm.com>
2017-06-24 15:10:03 -0700

    draw on canvas

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,30 @@
 module Main where
 
-import Prelude
+import Prelude (Unit, bind, discard)
+import Data.Maybe
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
+import Partial.Unsafe (unsafePartial)
 
-main :: ∀ eff. Eff (console :: CONSOLE | eff) Unit
-main = log "Purescript to the rescue!"
+import Graphics.Canvas (CANVAS)
+import Graphics.Canvas as C
+-- import Graphics.Drawing as D
+
+scene :: ∀ eff. C.Context2D -> Eff (canvas :: CANVAS | eff) C.Context2D
+scene ctx = do
+  ctx2 <- C.moveTo ctx 100.0 0.0
+  ctx3 <- C.lineTo ctx2 0.0 100.0
+  C.stroke ctx3
+
+
+innerMain :: ∀ eff. Partial => Eff (console :: CONSOLE, canvas :: CANVAS | eff) Unit
+innerMain = do
+  log "Purescript to the rescue!!"
+  Just canvas <- C.getCanvasElementById "viewport"  -- bad, fix
+  ctx <- C.getContext2D canvas
+  _ <- scene ctx
+  log "Done"
+
+main :: ∀ eff. Eff (console :: CONSOLE, canvas :: CANVAS | eff) Unit
+main = unsafePartial innerMain
