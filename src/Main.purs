d0a53334e77b319baabbfd3367f20aea7f54d44b
Greg Wiley <aztec.rex@jammm.com>
2017-06-24 16:29:08 -0700

    remove ppartial

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,6 @@
 module Main where
 
-import Prelude (Unit, bind, discard)
+import Prelude (Unit, unit, bind, discard, ($), (>>=), pure, when, (<<<), (>>>))
 import Data.Maybe
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
@@ -11,20 +11,28 @@ import Graphics.Canvas (CANVAS)
 import Graphics.Canvas as C
 -- import Graphics.Drawing as D
 
-scene :: ∀ eff. C.Context2D -> Eff (canvas :: CANVAS | eff) C.Context2D
+scene :: ∀ eff. C.Context2D -> Eff (canvas :: CANVAS | eff) Unit
 scene ctx = do
   ctx2 <- C.moveTo ctx 100.0 0.0
   ctx3 <- C.lineTo ctx2 0.0 100.0
-  C.stroke ctx3
+  _ <- C.stroke ctx3
+  pure unit
 
-
-innerMain :: ∀ eff. Partial => Eff (console :: CONSOLE, canvas :: CANVAS | eff) Unit
-innerMain = do
-  log "Purescript to the rescue!!"
-  Just canvas <- C.getCanvasElementById "viewport"  -- bad, fix
-  ctx <- C.getContext2D canvas
-  _ <- scene ctx
-  log "Done"
+viewport :: ∀ eff. Eff (console :: CONSOLE, canvas :: CANVAS | eff) (Maybe C.Context2D)
+viewport  = do
+  maybeCanvas <- C.getCanvasElementById "viewport"
+  case maybeCanvas of
+    Just canvas -> C.getContext2D canvas >>= (pure <<< Just)
+    _ -> pure Nothing
 
 main :: ∀ eff. Eff (console :: CONSOLE, canvas :: CANVAS | eff) Unit
-main = unsafePartial innerMain
+main = do
+  maybeContext <- viewport
+  case maybeContext of
+      Just context -> scene context
+      _ -> log "No canvas with id 'viewport'"
+  --
+  -- Just canvas <- C.getCanvasElementById "viewport"  -- bad, fix
+  -- ctx <- C.getContext2D canvas
+  -- _ <- scene ctx
+  log "Done"
