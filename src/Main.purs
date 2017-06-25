57aed7569acd992f30c9a9e72516723b566a43ae
Greg Wiley <aztec.rex@jammm.com>
2017-06-24 22:08:48 -0700

    spacebar controls power

--- src/Main.purs
+++ src/Main.purs
@@ -1,19 +1,35 @@
 module Main where
 
-import Prelude (Unit, negate, bind, discard, (/), (<>), (>>=), pure, ($), void, const)
+import Prelude (Unit, class Show, show, negate, bind, discard, (/), (<>), (>>=), pure, ($), void, const)
 import Data.Monoid (mempty)
 import Data.Maybe
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE, log, logShow)
 
 import Graphics.Canvas (CANVAS)
 import Graphics.Canvas as C
 import Graphics.Drawing as D
 
+import DOM (DOM)
+
+import Signal(Signal, (~>), runSignal, dropRepeats, foldp)
+import Signal.DOM (keyPressed)
 
 data Viewport = Viewport C.Context2D Number Number
 
-data Model = Ship Number
+data Model = Ship Boolean Number
+
+instance showModel :: Show Model where
+  show (Ship powered rotation) =
+    "ship " <>
+    show rotation <>
+    if powered then " powered" else ""
+
+modelLogic :: Boolean -> Model -> Model
+modelLogic power (Ship _ rot) = Ship power rot
+
+model :: Signal Boolean -> Signal Model
+model input = foldp modelLogic (Ship false 0.0) input
 
 shipShape :: D.Shape
 shipShape = D.closed [
@@ -40,14 +56,24 @@ shipDrawing powered = body <> if powered then tail else mempty
               trailShape
 
 
--- render :: ∀ eff. C.Context2D -> Model -> Eff (canvas :: CANVAS | eff) C.Context2D
--- render ctx model = do
---   let shape =
-
 scene :: ∀ eff. Viewport -> Eff (canvas :: CANVAS | eff) Unit
 scene (Viewport context width height) = do
   ctx2 <- C.translate {translateX: width / 2.0, translateY: height / 2.0} context
   D.render ctx2 $ shipDrawing true
+
+clearViewport :: ∀ eff. Viewport -> Eff (canvas :: CANVAS | eff) Unit
+clearViewport (Viewport context w h) = do
+  void $ C.setFillStyle "#FFFFFF" context
+  void $ C.fillRect context {x: 0.0, y: 0.0, w, h}
+
+
+
+render :: ∀ eff. Viewport -> Model -> Eff (canvas :: CANVAS | eff) Unit
+render viewport@(Viewport context width height) (Ship powered _) = C.withContext context $ do
+  void $ clearViewport viewport
+  void $ C.translate {translateX: width / 2.0, translateY: height / 2.0} context
+  D.render context $ shipDrawing powered
+
 
 viewport :: ∀ eff. Eff (console :: CONSOLE, canvas :: CANVAS | eff) (Maybe Viewport)
 viewport  = do
@@ -61,10 +87,15 @@ viewport  = do
       -- C.getContext2D canvas >>= (pure <<< Just)
     _ -> log "no canvas 'viewport'" >>= (const $ pure Nothing)
 
-main :: ∀ eff. Eff (console :: CONSOLE, canvas :: CANVAS | eff) Unit
+
+
+
+main :: ∀ eff. Eff (console :: CONSOLE, canvas :: CANVAS, dom :: DOM | eff) Unit
 main = do
-  maybeContext <- viewport
-  case maybeContext of
-      Just context -> void $ scene context
+  space <- keyPressed 32
+  maybeViewport <- viewport
+  case maybeViewport of
+      Just viewport -> runSignal $ model (dropRepeats space) ~> render viewport
       _ -> log "Cannot retrieve viewport canvas from document"
   log "Done"
+  runSignal $ model (dropRepeats space) ~> logShow
