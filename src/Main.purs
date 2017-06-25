133f689a529dcf0cd5be605b60f984a2fb3e2f50
Greg Wiley <aztec.rex@jammm.com>
2017-06-24 17:07:05 -0700

    cleanup

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,6 @@
 module Main where
 
-import Prelude (Unit, unit, bind, discard, (>>=), pure, (<<<), ($), void)
+import Prelude (Unit, bind, discard, (>>=), pure, (<<<), ($), void)
 import Data.Maybe
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
